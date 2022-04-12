6ef9e7d4f73e6bc7bc9db76c223953bf7e0d97e7
Simon Beirouti <hello@simonbeirouti.com>
2022-04-12 15:02:16 +1000

    Create the transaction schema

---
+++ lib/model/transaction_item.dart
@@ -0,0 +1,11 @@
+class TransactionItem {
+  String name;
+  double amount;
+  bool isExpense;
+
+  TransactionItem({
+    required this.name,
+    required this.amount,
+    required this.isExpense,
+  });
+}
